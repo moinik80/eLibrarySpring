d2e110699c71fc3dd0da7da655b4fea12149534a
moinik80 <moinik1980@yandex.by>
2015-03-29 17:47:33 +0300

    edit tiles and some .jspx pages

--- web/src/main/java/by/jd2/library/controller/IndexController.java
+++ web/src/main/java/by/jd2/library/controller/IndexController.java
@@ -1,8 +1,7 @@
 package by.jd2.library.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * Created by Иван on 29.03.2015.
