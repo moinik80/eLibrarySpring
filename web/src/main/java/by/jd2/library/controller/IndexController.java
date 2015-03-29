38e7edf7c7cbf633f70a6ecfec8347c5be11c1af
moinik80 <moinik1980@yandex.by>
2015-03-29 16:18:08 +0300

    try writing spring-mvc
    try add index page and controller

---
+++ web/src/main/java/by/jd2/library/controller/IndexController.java
@@ -0,0 +1,17 @@
+package by.jd2.library.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * Created by Иван on 29.03.2015.
+ */
+@Controller
+@RequestMapping("/index")
+public class IndexController {
+    @RequestMapping(value = "/index", method = RequestMethod.GET)
+    public String indexPage() {
+        return "index";
+    }
+}
